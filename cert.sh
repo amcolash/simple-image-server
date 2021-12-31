1eaa6a0929f33ff01d09b17f375162d4e8453151
Andrew McOlash <amcolash@gmail.com>
2021-12-30 18:30:54 -0600

    cert file renaming

--- cert.sh
+++ cert.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # List of IP addresses to use with cert
-IP_LIST="localhost 127.0.0.1 ::1 192.168.0.11"
+IP_LIST="localhost 127.0.0.1 ::1 192.168.0.11 192.168.1.135 192.168.1.146"
 
 # Make cert dir
 mkdir -p .cert
 
 # Make cert + key
-mkcert -key-file key.pem -cert-file cert.pem $IP_LIST
+mkcert -key-file privkey.pem -cert-file cert.pem $IP_LIST
 
 # Move files to cert dir
-mv key.pem cert.pem .cert
+mv privkey.pem cert.pem .cert
